16c3e26150ab996f45d25bfc81423fcc68bdc88f
hzy <1257664828@qq.com>
2021-12-06 10:26:08 +0800

    fix(packjson): 修改package.json配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import legacy from "@vitejs/plugin-legacy";
-import { Plugin } from "vite";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import legacy from '@vitejs/plugin-legacy'
+import { Plugin } from 'vite'
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,34 +11,44 @@ export default defineConfig({
     vue(),
     vueJsx(),
     legacy({
-      targets: ["defaults", "not IE 11"],
+      targets: ['defaults', 'not IE 11'],
     }),
     removePolyfileLegacyStrict(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'vant',
+          esModule: true,
+          resolveStyle: (name) => `vant/es/${name}/style`,
+        },
+      ],
+    }),
   ],
   server: {
     host: true,
   },
   resolve: {
     alias: {
-      "@": "/src/",
+      '@': '/src/',
     },
   },
-});
+  optimizeDeps: {},
+})
 
 // 移除 polyfill 的 use strict，修复在ios10 老设备上的不兼容。
 function removePolyfileLegacyStrict(): Plugin {
   return {
-    name: "vite-plugin-remove-strict-polyfills-legacy",
-    apply: "build",
+    name: 'vite-plugin-remove-strict-polyfills-legacy',
+    apply: 'build',
     generateBundle(opt, bundle) {
       for (const accessPath in bundle) {
         if (Object.prototype.hasOwnProperty.call(bundle, accessPath)) {
-          const obj = bundle[accessPath];
-          if (obj.type === "chunk" && obj.name === "polyfills-legacy") {
-            obj.code = obj.code.replace(/"use strict";/, "");
+          const obj = bundle[accessPath]
+          if (obj.type === 'chunk' && obj.name === 'polyfills-legacy') {
+            obj.code = obj.code.replace(/"use strict";/, '')
           }
         }
       }
     },
-  };
+  }
 }
