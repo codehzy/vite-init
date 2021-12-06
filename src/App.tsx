a3fa0a753cfc6d16724bb8c5ce98dab9a26ccfd6
hzy <1257664828@qq.com>
2021-12-06 15:03:14 +0800

    feat(vant): 引入vant组件库

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@
  * @Author: coderhzy
  * @Date: 2021-12-02 17:41:29
  * @Last Modified by: 填充
- * @Last Modified time: 2021-12-06 14:32:47
+ * @Last Modified time: 2021-12-06 15:02:07
  */
 
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 // import '@/style/index.css'
 import { a } from './ts/test'
 import '@/style/test.sass'
@@ -16,11 +16,18 @@ import { AppHeader } from './components'
 export default defineComponent({
   components: {},
   setup() {
+    const show = ref(false)
+    const showPopup = () => {
+      show.value = true
+    }
+
     return () => {
       return (
         <>
           <div class={`root ${classModule.moduleClass}`}>Hello {a.name} </div>
           <AppHeader />
+          <vanCell onclick={showPopup}>展示弹出层</vanCell>
+          <vanPopup v-model:show={show.value}>内容</vanPopup>
         </>
       )
     }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App'
+import Vant, { Popup } from 'vant'
+import 'vant/lib/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Vant).use(Popup).mount('#app')
